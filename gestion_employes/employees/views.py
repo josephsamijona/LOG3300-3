ef872e4e5ae44a4fe954085f459acd16a68c1bd7
Samuel Jonathan JOSEPH <isteah.josephsamuel@gmail.com>
2024-04-07 15:20:53 -0400

    static file

--- gestion_employes/employees/views.py
+++ gestion_employes/employees/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def gestion(request):
+    return render(request, 'gestion.html')
+
+def employee(request):
+    return render(request, 'employee.html')
+
+def ajouter_departement(request):
+    return render(request, 'ajouter-departement.html')
+
+def ajouter_employee(request):
+    return render(request, 'ajouter_employee.html')
--- gestion_employes/gestion_employes/settings.py
+++ gestion_employes/gestion_employes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,7 +121,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+# settings.py
+
+# Définition du répertoire où Django cherchera les fichiers statiques
+STATIC_URL = '/static/'
+
+# Ajout du chemin absolu vers le répertoire contenant vos fichiers statiques
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+# settings.py
+
+# Définition du répertoire où Django collectera les fichiers statiques en mode production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
