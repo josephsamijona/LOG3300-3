7d9e607fb2b1187fa3b4b7db5df2477df70310a9
Samuel Jonathan JOSEPH <isteah.josephsamuel@gmail.com>
2024-04-07 11:06:58 -0400

    setting

--- gestion_employes/gestion_employes/settings.py
+++ gestion_employes/gestion_employes/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "employees",
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = "gestion_employes.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME":  'ecole',
+        "USER": 'safira',
+        "PASSWORD": 'monnaie1',
+        "HOST": 'localhost',
+        "PORT": 3306,
     }
 }
 
